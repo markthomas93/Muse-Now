1174daf055d68c090603504d4403c25af1bb6b1b
musesum <warren@muse.com>
2018-07-05 17:25:52 -0700

    refactor DotMenu

--- Shared/Files/TreeNodes+Tour.swift
+++ Shared/Files/TreeNodes+Tour.swift
@@ -8,64 +8,61 @@
 
 import Foundation
 
-extension TreeNodes {
+extension Tour {
 
-//    func backupSettings() {
-//
-//        let encoder = JSONEncoder()
-//        encoder.outputFormatting = .prettyPrinted
-//
-//        if let data = try? encoder.encode(settings) {
-//            let backName = fileName+".backup"
-//            Log ("⧉⧉ Settings::backupSettings to file: \(backName)")
-//            let _ = saveData(data, backName)
-//        }
-//    }
-//
-//    func restoreFromBackup(_ done: @escaping () -> Void) {
-//
-//        let backName = fileName+".backup"
-//        let fm = FileManager.default
-//        fm.delegate = self
-//
-//        let fileUrl = FileManager.documentUrlFile(fileName)
-//        let backUrl = FileManager.documentUrlFile(backName)
-//
-//        do {
-//            if fm.fileExists(atPath: backUrl.path) {
-//                if fm.fileExists(atPath: fileUrl.path) {
-//                    try fm.removeItem(at: fileUrl)
-//                }
-//                try fm.moveItem(at:backUrl, to:fileUrl)
-//            }
-//            Log ("⧉⧉ Settings::restoreFromBackup overwrite from backup file: \(backName)")
-//            done()
-//        }
-//        catch {
-//            Log ("⧉⧉ Settings::restoreFromBackup no backup file: \(backName)")
-//            done()
-//        }
-//    }
+    //    func backupSettings() {
+    //
+    //        let encoder = JSONEncoder()
+    //        encoder.outputFormatting = .prettyPrinted
+    //
+    //        if let data = try? encoder.encode(settings) {
+    //            let backName = fileName+".backup"
+    //            Log ("⧉⧉ Settings::backupSettings to file: \(backName)")
+    //            let _ = saveData(data, backName)
+    //        }
+    //    }
+    //
+    //    func restoreFromBackup(_ done: @escaping () -> Void) {
+    //
+    //        let backName = fileName+".backup"
+    //        let fm = FileManager.default
+    //        fm.delegate = self
+    //
+    //        let fileUrl = FileManager.documentUrlFile(fileName)
+    //        let backUrl = FileManager.documentUrlFile(backName)
+    //
+    //        do {
+    //            if fm.fileExists(atPath: backUrl.path) {
+    //                if fm.fileExists(atPath: fileUrl.path) {
+    //                    try fm.removeItem(at: fileUrl)
+    //                }
+    //                try fm.moveItem(at:backUrl, to:fileUrl)
+    //            }
+    //            Log ("⧉⧉ Settings::restoreFromBackup overwrite from backup file: \(backName)")
+    //            done()
+    //        }
+    //        catch {
+    //            Log ("⧉⧉ Settings::restoreFromBackup no backup file: \(backName)")
+    //            done()
+    //        }
+    //    }
 
 
     func prepareDemoSettings() {
-        
-        #if os(iOS)
-            Log ("⧉ Settings::\(#function)")
 
-            Show.shared.setupBeforeDemo()
-            Say.shared.setupBeforeDemo()
-            Settings.shared.setupBeforeDemo()
+        Log ("⧉ \(#function)")
 
-            TreeNodes.shared.root?.refreshNodeCells()
-            PagesVC.shared.menuVC?.tableView.reloadData()
-            Actions.shared.doAction(.refresh)
-        #endif
+        Show.shared.setupBeforeDemo()
+        Say.shared.setupBeforeDemo()
+        Settings.shared.setupBeforeDemo()
+
+        TreeNodes.shared.root?.refreshNodeCells()
+        PagesVC.shared.menuVC?.tableView.reloadData()
+        Actions.shared.doAction(.refresh)
     }
 
     func finishDemoSettings() {
-        #if os(iOS)
-        Log ("⧉ Settings::\(#function)")
+        Log ("⧉ \(#function)")
         Show.shared.restoreAfterDemo()
         Say.shared.restoreAfterDemo()
         Settings.shared.restoreAfterDemo()
@@ -73,7 +70,6 @@ extension TreeNodes {
         TreeNodes.shared.root?.refreshNodeCells()
         PagesVC.shared.menuVC?.tableView.reloadData()
         Actions.shared.doAction(.refresh)
-        #endif
     }
 
 }
